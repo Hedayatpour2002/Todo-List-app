1dc8d925fc3d8409011b09796beddb7f2b5c6ffd
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2024-08-14 07:16:24 +0800

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,35 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      gray: {
+        100: "#F2F2F2",
+        200: "#D9D9D9",
+        300: "#808080",
+        400: "#333333",
+        500: "#262626",
+        600: "#1A1A1A",
+        700: "#0D0D0D",
+      },
+
+      danger: "#E25858",
+
+      purple: {
+        DEFAULT: "#8284FA",
+        dark: "#5E60CE",
+      },
+
+      blue: {
+        DEFAULT: "#4EA8DE",
+        dark: "#1E6F9F",
+      },
+    },
+    container: {
+      padding: '2rem',
+    },
     extend: {},
   },
   plugins: [],
